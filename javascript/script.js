9c272de9df9683c4ecca68892fedac31357a312e
ravi <rabibanik014@gmail.com>
2022-06-14 17:30:49 +0530

    add obstacle and minor changes

--- javascript/script.js
+++ javascript/script.js
@@ -16,4 +16,29 @@ document.onkeydown = function(up){
            tyre.classList.remove('jumptyre');
        }, 500);
     }
+
+    if (up.keyCode == 38){
+       jump = document.querySelector('.carbody ');
+       jump.classList.add('jumpcar');
+       setTimeout(() => {
+           jump.classList.remove('jumpcar');
+       }, 500);
+    }
+
+    if (up.keyCode == 38){
+       tyre = document.querySelector('.tyre');
+       tyre.classList.add('jumptyre');
+       setTimeout(() => {
+           tyre.classList.remove('jumptyre');
+       }, 500);
+    }
+
+    if (up.keyCode == 72){
+    var audio = document.createElement('audio');
+    audio.setAttribute('src','hornsound.mp3');
+    audio.play();
+
+    }
 }
+
+
