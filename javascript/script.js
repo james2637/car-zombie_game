1addfdcc0a2729ef4e4ba15af5798bb53210888a
ravi <rabibanik014@gmail.com>
2022-07-17 21:33:44 +0530

    game done

--- javascript/script.js
+++ javascript/script.js
@@ -1,120 +1,89 @@
-document.onkeydown = function(key){
-    console.log("key code is : ", key.keyCode)
+score = 0;
+document.onkeydown = function (key) {
+    // console.log("key code is : ", key.keyCode)
 
-    if (key.keyCode == 32){
-       jump = document.getElementById('carcng');
-       jump.classList.add('jumpcar');
-       setTimeout(() => {
-           jump.classList.remove('jumpcar');
-       }, 600);
+    if (key.keyCode == 32) {
+        jump = document.getElementById('carcng');
+        jump.classList.add('jumpcar');
+        setTimeout(() => {
+            jump.classList.remove('jumpcar');
+        }, 500);
     }
 
-    if (key.keyCode == 32){
-       tyre = document.querySelector('.tyre');
-       tyre.classList.add('jumptyre');
-       setTimeout(() => {
-           tyre.classList.remove('jumptyre');
-       }, 600);
+    if (key.keyCode == 32) {
+        tyre = document.querySelector('.tyre');
+        tyre.classList.add('jumptyre');
+        setTimeout(() => {
+            tyre.classList.remove('jumptyre');
+        }, 500);
     }
 
-    if (key.keyCode == 38){
-       jump = document.getElementById('carcng');
-       jump.classList.add('jumpcar');
-       setTimeout(() => {
-           jump.classList.remove('jumpcar');
-       }, 600);
+    if (key.keyCode == 38) {
+        jump = document.getElementById('carcng');
+        jump.classList.add('jumpcar');
+        setTimeout(() => {
+            jump.classList.remove('jumpcar');
+        }, 500);
     }
 
-    if (key.keyCode == 38){
-       tyre = document.querySelector('.tyre');
-       tyre.classList.add('jumptyre');
-       setTimeout(() => {
-           tyre.classList.remove('jumptyre');
-       }, 600);
+    if (key.keyCode == 38) {
+        tyre = document.querySelector('.tyre');
+        tyre.classList.add('jumptyre');
+        setTimeout(() => {
+            tyre.classList.remove('jumptyre');
+        }, 500);
     }
 
-    if (key.keyCode == 72){
-    var audio = document.createElement('audio');
-    audio.setAttribute('src','hornsound.mp3');
-    audio.play();
-
-    }
 }
 
-// function changecar() {
-//     console.log('the button was clicked');
 
-//     var image = document.getElementById('carcng');
-//     if (image.src.match("car-body1")) {
-//         image.src ="img/car-body.png";
-//         image.classList.replace("carbody","carbody2");
-//     }
-//     else{
-//         image.src ="img/car-body1.png";
-//         image.classList.replace("carbody2","carbody");
-//     }
+intervalid = setInterval(() => {
 
-// }
-
-    setInterval(() => {
-
-     car = document.getElementById('carcng') ;
-     obstacle = document.querySelector('.obstacle');
-     gameOver = document.querySelector('.gameover');
-
-     carComStyleLeft = parseInt( window.getComputedStyle(car).getPropertyValue('left'));
-     carComStyleTop = parseInt( window.getComputedStyle(car).getPropertyValue('top'));
-     obsComStyleLeft = parseInt( window.getComputedStyle(obstacle).getPropertyValue('left'));
-     obsComStyleTop = parseInt( window.getComputedStyle(obstacle).getPropertyValue('top'));
-    
-        // console.log(carComStyleLeft);
-        // console.log(carComStyleTop);
-        // console.log(obsComStyleLeft);
-        // console.log(obsComStyleTop);
-
-    offsetx = Math.abs(carComStyleLeft-obsComStyleLeft) ; 
-    offsety = Math.abs(carComStyleTop-obsComStyleTop) ;
-    
-    // console.log(offsetx, offsety);
-
-    if (offsetx < 356 && offsety < 130) {
-        gameOver.style.visibility = "visible";
-    }
-
-    else{
-        gameOver.style.visibility = "hidden"; 
-    }
-}, 300);
-    
-    
-//         ANOTHER WAY FOR CAR JUMP
-
-// document.addEventListener('keydown',function(e) {
-//     if (e.code == 'ArrowUp') {
-//         jump = document.querySelector('.carbody ');
-//            jump.classList.add('jumpcar');
-//            setTimeout(() => {
-//                jump.classList.remove('jumpcar');
-//            }, 500);
-//     }
-
-//     if (e.code == 'ArrowUp') {
-//         tyre = document.querySelector('.tyre');
-//           tyre.classList.add('jumptyre');
-//           setTimeout(() => {
-//               tyre.classList.remove('jumptyre');
-//           }, 500);
-//     }
-// })
-
-
-
- setInterval(() => {
-    car = document.querySelector('.carbody ');
-    gameOver = document.querySelector('.gameover');
+    car = document.getElementById('carcng');
     obstacle = document.querySelector('.obstacle');
+    gameOver = document.querySelector('.gameover');
+    tyre1 = document.querySelector('.tyre1');
+    tyre2 = document.querySelector('.tyre2');
+    tyre = document.querySelector('.tyre');
+    background = document.querySelector('.background');
+    
 
-    
-    
- }, 100);
+    carComStyleTop = parseInt(window.getComputedStyle(car).getPropertyValue('top'));
+    obsComStyleRight = parseInt(window.getComputedStyle(obstacle).getPropertyValue('right'));
+    obsComStyleTop = parseInt(window.getComputedStyle(obstacle).getPropertyValue('top'));
+
+
+    offsety = Math.abs(carComStyleTop - obsComStyleTop);
+
+    if (obsComStyleRight > 726 && obsComStyleRight < 1088 && offsety < 100) {
+        // console.log("gya");
+        gameOver.style.visibility = "visible";
+        obstacle.classList.add('obstaclestop');
+        tyre1.classList.remove('tyre1-ani');
+        tyre2.classList.remove('tyre2-ani');
+        background.classList.remove('background-ani');
+        car.classList.add('jumpstop');
+        tyre.classList.add('jumpstop');
+        clearInterval(intervalid);
+        if (localStorage.getItem("score") < score) {
+
+            localStorage.setItem("score", score);
+        }
+    }
+
+    else {
+        score += 1;
+        // console.log(score);
+        document.querySelector('.score').innerText = 'Your score: ' + score;
+    }
+
+    document.querySelector('.Gm-score').innerText = 'Your score: ' + score; 
+    // console.log(localStorage.getItem("score"));
+    document.querySelector('.hgscore').innerText = 'High score :' + (localStorage.getItem("score"))
+
+
+}, 100);
+
+
+
 
